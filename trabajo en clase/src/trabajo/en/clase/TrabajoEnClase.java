e8106b0b9aeb7e50c5f71a9a3b331c1a53fc4354
Sebastiansolarte21 <ssolarte21@outlook.com>
2024-05-29 20:48:06 -0600

    trabajo
    trabajo en clase#2

---
+++ trabajo en clase/src/trabajo/en/clase/TrabajoEnClase.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabajo.en.clase;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Administrador
+ */
+public class TrabajoEnClase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int resultado;
+        resultado = Integer.parseInt(JOptionPane.showInputDialog("Digite su nota"));
+        if (resultado <= 70){
+        }
+        
+        
+        
+    }
+    
+}
